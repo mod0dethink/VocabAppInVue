bf25edf2ec462bbe9950bcb02cc78c015f605699
mod0dethink <tanakaryuto1994@gmail.com>
2024-02-01 23:42:52 +0900

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <input v-model="inputValue" placeholder="何か入力してください">
+      <button @click="saveToLocalStorage">保存</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        inputValue: '', // ユーザーの入力を保持するためのデータプロパティ
+      };
+    },
+    methods: {
+      saveToLocalStorage() {
+        // LocalStorageにユーザーの入力を保存
+        localStorage.setItem('userInput', this.inputValue);
+        alert('保存しました: ' + this.inputValue);
+        // 保存後、入力フィールドをクリア
+        this.inputValue = '';
+      }
+    }
+  }
+  </script>
